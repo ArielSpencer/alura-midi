70adbf73d6fdb2544328679e32a23c1e4cfda4b9
Ariel Spencer <arielspencer.tech@gmail.com>
2024-06-14 00:50:05 -0300

    Refatorar js

--- main.js
+++ main.js
@@ -1,53 +1,17 @@
-function tocarPom() {
-        document.querySelector('#som_tecla_pom').play();    
+function tocarSom(idTeclaAudio) {
+    document.querySelector(idTeclaAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocarPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocarClap() {
-        document.querySelector('#som_tecla_clap').play();    
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1]
+
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function () {
+        tocarSom(idAudio);
+    }
+
 }
-
-document.querySelector('.tecla_clap').onclick = tocarClap;
-
-function tocarTim() {
-        document.querySelector('#som_tecla_tim').play();    
-}
-
-document.querySelector('.tecla_tim').onclick = tocarTim;
-
-function tocarPuff() {
-        document.querySelector('#som_tecla_puff').play();    
-}
-
-document.querySelector('.tecla_puff').onclick = tocarPuff;
-
-function tocarSplash() {
-        document.querySelector('#som_tecla_splash').play();    
-}
-
-document.querySelector('.tecla_splash').onclick = tocarSplash;
-
-function tocarToim() {
-        document.querySelector('#som_tecla_toim').play();    
-}
-
-document.querySelector('.tecla_toim').onclick = tocarToim;
-
-function tocarPsh() {
-        document.querySelector('#som_tecla_psh').play();    
-}
-
-document.querySelector('.tecla_psh').onclick = tocarPsh;
-
-function tocarTic() {
-        document.querySelector('#som_tecla_tic').play();    
-}
-
-document.querySelector('.tecla_tic').onclick = tocarTic;
-
-function tocarTom() {
-        document.querySelector('#som_tecla_tom').play();    
-}
-
-document.querySelector('.tecla_tom').onclick = tocarTom;
